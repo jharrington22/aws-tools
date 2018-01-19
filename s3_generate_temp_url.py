ab04c7291d0d27a45b49d0b257e3c6a9c5f78e18
jharrington22 <james@harrington.net.au>
2018-01-19 11:45:51 -0500

    Update boto to v3 and allow signature version to be specified

--- s3_generate_temp_url.py
+++ s3_generate_temp_url.py
@@ -1,4 +1,5 @@
-import boto
+import boto3
+from botocore.client import Config
 import time
 import argparse
 
@@ -10,21 +11,26 @@ if __name__ == "__main__":
     parser.add_argument('-f', '--file-name', action="store", help="S3 file (Object) name")
     parser.add_argument('-e', '--expiry', action="store", help="Expiry time of URL in seconds")
     parser.add_argument('-b', '--bucket', action="store", help="S3 Bucket name")
-
+    parser.add_argument('-s', '--signature-version', action="store", help="S3 signature version eg, -s v4")
     args = parser.parse_args()
 
     expire_timeout = int(args.expiry)
-
-    key_name = args.file_name
-
-    conn = boto.connect_s3()
-
-    bucket = conn.get_bucket(args.bucket)
-
-    key = bucket.get_key(key_name)
-
+    # Get the service client.
+    if args.signature_version == "v4":
+        conn = boto3.client('s3', config=Config(signature_version='s3v4'))
+    else:
+        conn = boto3.client('s3')
+    # Generate the URL to get 'file-name' from 'bucket-name'
+    params = {
+        'Bucket': args.bucket,
+        'Key': args.file_name
+    }
+    url = conn.generate_presigned_url(
+                                        ClientMethod='get_object',
+                                        Params=params,
+                                        ExpiresIn=expire_timeout
+    )
     expiry_date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time() + expire_timeout))
-
-    temp_url = key.generate_url(expire_timeout)
-
-    print("Your file: {} is ready to download it will expire on {}\nLink: {}\n".format(key_name, expiry_date, temp_url))
+    print("Your file: {} is ready to download it will expire on {}\nLink: {}\n".format(args.file_name,
+                                                                                       expiry_date,
+                                                                                       url))
